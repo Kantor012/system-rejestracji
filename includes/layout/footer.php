cb21a52836444f53c0c2a6b9f3c9a24c06460fb2
Kantor012 <47494655+Kantor012@users.noreply.github.com>
2023-01-04 08:26:04 +0100

    Delete includes directory

--- includes/layout/footer.php
+++
@@ -1,6 +0,0 @@
-    </main>
-
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"></script>
-    <script defer src="/system-rejestracji/public/dist/script.js"></script>
-  </body>
-</html>
